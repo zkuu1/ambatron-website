8d1735fb4971cdc4992b9d83eb7f5af7eadf2cfd
Zkuu <142339560+zkuu1@users.noreply.github.com>
2025-04-11 20:49:03 +0700

    [+] make repo

---
+++ constants/index.ts
@@ -0,0 +1,234 @@
+import { link } from "fs";
+
+export const Skill_data = [
+    {
+      skill_name: "classic",
+      Image: "/plane/classic_plane.png",
+      width: 150,
+      height: 150,
+    },
+    {
+      skill_name: "core x6",
+      Image: "/plane/core_x6.png",
+      width: 150,
+        height: 150,
+    },
+    {
+      skill_name: "liz_blue",
+      Image: "/plane/liz_blue.png",
+      width: 150,
+      height: 150,
+    },
+    {
+      skill_name: "Nusei",
+      Image: "/plane/nusei_15.png",
+      width: 150,
+        height: 150,
+    },
+    {
+      skill_name: "React",
+      Image: "/plane/shak_magma.png",
+      width: 150,
+        height: 150,
+    },
+    {
+      skill_name: "Redux",
+      Image: "/plane/space_defender.png",
+      width: 150,
+        height: 150,
+    },
+    {
+      skill_name: "React Query",
+      Image: "/plane/wof.png",
+      width: 150,
+        height: 150,
+    },
+    
+  
+  ];
+  
+  export const Socials = [
+    {
+      name: "Discord",
+      src: "/instagram.svg",
+      link: "https://www.instagram.com/sotganapi.dev?igsh=MXU4cnY4NHVsazVtMA%3D%3D",
+    },
+    {
+      name: "Facebook",
+      src: "/facebook.svg",
+      link: "/",
+    },
+    {
+      name: "Discord",
+      src: "/discord.svg",
+      link: "https://discord.gg/92FfJnkf",
+    },
+  ];
+  
+  
+  
+  export const Frontend_skill = [
+      {
+        skill_name: "Type Script",
+        Image: "/boss/boss1.png",
+        width: 150,
+        height: 150,
+      },
+      {
+        skill_name: "Next js 13",
+        Image: "/boss/boss2.png",
+        width: 150,
+        height: 150,
+      },
+      {
+        skill_name: "Framer Motion",
+        Image: "/boss/boss3.png",
+        width: 150,
+        height: 150,
+      },
+      {
+        skill_name: "Stripe Payment",
+        Image: "/boss/boss4.png",
+        width: 150,
+        height: 150,
+      },
+      {
+        skill_name: "Node js",
+        Image: "/boss/boss5.png",
+        width: 150,
+        height: 150,
+      },
+      {
+        skill_name: "Mongo db",
+        Image: "/boss/boss6.png",
+        width: 150,
+        height: 150,
+      },
+      {
+        skill_name: "Mongo db",
+        Image: "/boss/boss7.png",
+        width: 150,
+        height: 150,
+      },
+      {
+        skill_name: "Mongo db",
+        Image: "/boss/boss8.png",
+        width: 150,
+        height: 150,
+      },
+      {
+        skill_name: "Mongo db",
+        Image: "/boss/boss9.png",
+        width: 150,
+        height: 150,
+      },
+      {
+        skill_name: "Mongo db",
+        Image: "/boss/boss10.png",
+        width: 150,
+        height: 150,
+      },
+      {
+        skill_name: "Mongo db",
+        Image: "/boss/boss11.png",
+        width: 150,
+        height: 150,
+      },
+
+  ];
+  
+  export const Backend_skill = [
+    {
+      skill_name: "Node js",
+      Image: "/node-js.png",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Express js",
+      Image: "/express.png",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Mongo db",
+      Image: "/mongodb.png",
+      width: 40,
+      height: 40,
+    },
+    {
+      skill_name: "Fire base",
+      Image: "/Firebase.png",
+      width: 55,
+      height: 55,
+    },
+    {
+      skill_name: "Postger SQL",
+      Image: "/postger.png",
+      width: 70,
+      height: 70,
+    },
+    {
+      skill_name: "My SQL",
+      Image: "/mysql.png",
+      width: 70,
+      height: 70,
+    },
+    {
+      skill_name: "Prisma",
+      Image: "/prisma.webp",
+      width: 70,
+      height: 70,
+    },
+    {
+      skill_name: "Graphql",
+      Image: "/graphql.png",
+      width: 80,
+      height: 80,
+    },
+  ];
+  
+  export const Full_stack = [
+    {
+      skill_name: "React Native",
+      Image: "/ReactNative .png",
+      width: 70,
+      height: 70,
+    },
+    {
+      skill_name: "Tauri",
+      Image: "/tauri.svg",
+      width: 70,
+      height: 70,
+    },
+    {
+      skill_name: "Docker",
+      Image: "/docker.webp",
+      width: 70,
+      height: 70,
+    },
+  
+    {
+      skill_name: "Figma",
+      Image: "/figma.png",
+      width: 50,
+      height: 50,
+    },
+  
+  ];
+  
+  
+  
+  export const Other_skill = [
+    {
+      skill_name: "Go",
+      Image: "/go.png",
+      width: 60,
+      height: 60,
+    },
+  ];
+  
+  
+  
+  
+  
